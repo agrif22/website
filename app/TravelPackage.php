0a8b88ee28a88372164ce5484d8bf4d171777d1b
agrif22 <agoesrifqi@gmail.com>
2021-03-12 00:44:52 +0700

    Membuat Crud di Admin Travek Package

---
+++ app/TravelPackage.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TravelPackage extends Model
+{
+
+    // protected $table = 'travel_packages';
+    protected $fillable = [
+        'title', 'slug', 'location', 'about', 'featured_event',
+        'language', 'foods', 'departure_date', 'duration',
+        'type', 'price'
+    ];
+    use SoftDeletes;
+    protected $hidden = [];
+
+}
